2bcb1cb76fee72fa38044ecebc5722fef9033441
maldrek3103 <cpisanu96@gmail.com>
2021-09-02 17:15:58 +0200

    printed axios response on page 10 times

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-
+    emails: [],
   },
   methods: {},
-
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+        const email = res.data.response;
+        this.emails.push(email);
+      });
+    };
+  },
 });
